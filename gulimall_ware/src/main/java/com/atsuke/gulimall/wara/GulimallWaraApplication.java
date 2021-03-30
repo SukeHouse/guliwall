3d4772c332f9e0c5ad7058d0b671938429fb3444
atsuke <maolitz2012@sina.com>
2021-03-30 21:24:23 +0800

    谷粒商城加购第一次提交

---
+++ gulimall_ware/src/main/java/com/atsuke/gulimall/wara/GulimallWaraApplication.java
@@ -0,0 +1,13 @@
+package com.atsuke.gulimall.wara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWaraApplication.class, args);
+    }
+
+}
---
+++ gulimall_ware/src/test/java/com/atsuke/gulimall/wara/GulimallWaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsuke.gulimall.wara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
